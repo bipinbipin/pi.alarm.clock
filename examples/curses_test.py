5086e3be56653c228608e18e8f9b29b0561e79ed
bipin <bipinbipin@gmail.com>
2018-01-02 14:39:58 -0600

    examples

--- examples/curses_test.py
+++ examples/curses_test.py
@@ -9,16 +9,34 @@ def main(stdscr):
     while True:
         # Store the key value in the variable `c`
         c = stdscr.getch()
-        print(c);
-        print(curses.keyname(c))
+        # print(c);
+        # print(curses.keyname(c))
         # Clear the terminal
         # stdscr.clear()
-        # if c == ord('a'):
-        #     stdscr.addstr("You pressed the 'a' key.")
-        # elif c == curses.KEY_UP:
-        #     stdscr.addstr("You pressed the up arrow.")
-        # else:
-        #     stdscr.addstr("This program doesn't know that key.....")
+        if c == ord('1'):
+            stdscr.addstr("You pressed the '1' key.")
+        elif c == ord('2'):
+            stdscr.addstr("You pressed the '2' key.")
+        elif c == ord('3'):
+            stdscr.addstr("You pressed the '3' key.")
+        elif c == ord('4'):
+            stdscr.addstr("You pressed the '4' key.")
+        elif c == ord('5'):
+            stdscr.addstr("You pressed the '5' key.")
+        elif c == ord('6'):
+            stdscr.addstr("You pressed the '6' key.")
+        elif c == ord('7'):
+            stdscr.addstr("You pressed the '7' key.")
+        elif c == ord('8'):
+            stdscr.addstr("You pressed the '8' key.")
+        elif c == ord('9'):
+            stdscr.addstr("You pressed the '9' key.")
+        elif c == ord('0'):
+            stdscr.addstr("You pressed the '0' key.")
+        elif c == curses.KEY_ENTER:
+            stdscr.addstr("You pressed the 'Enter' key.")
+        else:
+            stdscr.addstr("This program doesn't know that key.....")
 
 def isNumber(c):
     c.isdigit()
