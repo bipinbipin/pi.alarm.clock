be7dd6f93b5ade9f68c375633a9c0820768664c4
bipin <bipinbipin@gmail.com>
2018-09-07 15:58:16 -0500

    encoder to set time

--- examples/alarm_clock.py
+++ examples/alarm_clock.py
@@ -56,13 +56,9 @@ class AlarmClock:
     _MODE_DISPLAY_TIME = False
 
     def display_minutes(self):
-        display_number = format(self.MINUTE_BUFFER, '02d')
-        print(display_number)
-        print(int(str(display_number)[:1]))
-        print(int(str(display_number)[:2]))
-
-        self.segment.set_digit(2, int(str(display_number)[:1]))
-        self.segment.set_digit(3, int(str(display_number)[:2]))
+        display_string = str(format(self.MINUTE_BUFFER, '02d'))
+        self.segment.set_digit(2, int(display_string[0]))
+        self.segment.set_digit(3, int(display_string[1]))
         self.segment.set_decimal(2, True)
         self.segment.set_decimal(3, True)
         self.segment.write_display()
